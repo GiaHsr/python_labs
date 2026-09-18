dbd78e359fe64321adc622c3dfbb3bff58029154
Adelinasabirova <adelinasabirova@MacBook-Air-Adelinasabirova.local>
2026-09-18 14:26:19 +0300

    5

--- src/05_initials_and_len.py
+++ src/05_initials_and_len.py
@@ -1,9 +1,5 @@
-n = int(input())
-o = 0
-z = 0
-for i in range(n):
-    s = input().split()
-    if s[-1] == 'True': o += 1
-    else: z += 1
+f = input('ФИО: ')
+f = f.split()
 
-print(o, z)
+print('Инициалы:', f[0][0] + f[1][0] + f[2][0] + '.')
+print('Длина (символов):', len(f[0]) + len(f[1]) + len(f[2]) + 2)
